64590f4f60ad219083dfd21b793006e7fb6addd3
Lee Gent <lee@leegent.net>
2015-09-21 12:01:04 +0100

    Fixed #23 - now not possible to create multiple instances of your GroupMemberProfile by repeatedly joining the group with the join link.

--- group/urls.py
+++ group/urls.py
@@ -4,6 +4,6 @@ import views
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views.group, name="group"),
     url(r'^create/$', views.creategroup, name="creategroup"),
-    url(r'^join/(?P<secret>[\w\d-]+)/$', views.joingroup, name="joingroup"),
+    url(r'^join/(?P<secret>[\w\d-]+)/$', views.join_group, name="joingroup"),
 
 ]
--- group/views.py
+++ group/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render, get_object_or_404
 from .models import GroupProfile, GroupMemberProfile
 
@@ -11,15 +11,20 @@ def group(request, pk):
     return render(request, "group/group_detail.html", {"profile": groupprofile})
 
 
-
 @login_required
-def joingroup(request, secret):
+def join_group(request, secret):
     user = request.user
     groupprofile = get_object_or_404(GroupProfile, secret_key=secret)
     group = groupprofile.group
 
+    member_profile_exists = GroupMemberProfile.objects.filter(group=groupprofile, member=user).exists()
+
     if 'confirm' in request.GET:
         # Dead easy. Add our request user to the group and quit.
+        # ...if they're not in there already!
+        if member_profile_exists:
+            return HttpResponseBadRequest("You are already a member of this Group!")
+
         group.user_set.add(user)
 
         group_member_profile = GroupMemberProfile()
@@ -35,7 +40,12 @@ def joingroup(request, secret):
 
         return HttpResponseRedirect(reverse('dashboard'))
 
-    return render(request, "group/group_join.html", {"profile": groupprofile})
+    if member_profile_exists:
+        template = "group/group_already_joined.html"
+    else:
+        template = "group/group_join.html"
+
+    return render(request, template, {"profile": groupprofile})
 
 
 def creategroup(request):
